cbf88faafd1e05e296e2ad0e5b502924d7b05c26
djinN999 <94804415+djinN999@users.noreply.github.com>
2021-11-22 22:48:09 +0100

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace part2_3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int zahl1, zahl2, quotient, modulo;
+			
+			Console.WriteLine("Geben Sie den Dividenden ein: ");
+			zahl1 = Convert.ToInt32(Console.ReadLine());
+				
+			Console.WriteLine("Geben Sie den Divisor ein: ");
+			zahl2 = Convert.ToInt32(Console.ReadLine());
+			while (zahl2 == 0)
+			{		
+				Console.WriteLine("Durch 0 kann nicht geteilt werden, geben Sie einen gültigen Divisot ein: ");
+				zahl2 = Convert.ToInt32(Console.ReadLine());
+			}
+
+			quotient = zahl1 / zahl2;
+			modulo = zahl1 % zahl2;
+
+			Console.WriteLine("Ergebnis: {0}", quotient);
+			Console.WriteLine("Der Rest der Division ist: {0}", modulo);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
